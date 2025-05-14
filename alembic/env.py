a8d92567922768eb58a60f17f24cd4d62d90db02
argalusmp <argalus03@gmail.com>
2025-05-15 00:13:03 +0700

    Minor: use manual OS to get env not using getenv cause error in server DO

--- alembic/env.py
+++ alembic/env.py
@@ -3,17 +3,13 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 import os
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
 # Set the database URL from environment variables
-config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
+config.set_main_option('sqlalchemy.url', os.environ.get('DATABASE_URL'))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
